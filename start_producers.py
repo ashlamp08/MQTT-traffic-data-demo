c5b347800d443e00edbbe9f059f2f1e63e318693
AtteRouhe <atte.rouhe@gmail.com>
2022-02-23 16:07:44 +0200

    Add comments

--- start_producers.py
+++ start_producers.py
@@ -11,26 +11,37 @@ import time
 
 
 def start_producer(q):
+    """Start the event (traffic data) producer
+
+    Args:
+        q (_type_): Reference to the main queue
+    """
+
     # read files to list
     f = []
     for (dirpath, dirnames, filenames) in walk("./sample_data"):
         f.extend(filenames)
 
-    # sort files
+    # sort files (so timestamps in correct order)
     f.sort()
 
-    # add all timestamps to queue
+    # loop all filenames
     for filename in f:
 
+        # extract timestamp, datetime from filename
         timestamp = int(filename.replace(".json", ""))
         date_and_time = datetime.datetime.fromtimestamp(timestamp)
 
         print(f"Producer: queue all data from [{date_and_time}]")
+
+        # open actual file, and queue each roads data
         with open(f"./sample_data/{filename}", "r") as f:
             data = json.loads(f.read())
             for road in data:
-                # put all timestamps data into queue
                 q.put(road)
+
+        # TODO: assert 50 second passing
+        # simulate passing of time
         time.sleep(10)
 
 
@@ -44,6 +55,7 @@ def process_queue_items(p, q, client):
     """
 
     print(p, "starting")
+    # keep processing data from the queue when available
     while True:
         message = str(q.get())
         print(f"{p} -> {message[:100]} ..")
@@ -53,14 +65,22 @@ def process_queue_items(p, q, client):
 
 
 def start_publishers(q, n: int):
+    """Start the publishers (consumers) of traffic data
 
-    # start
+    Args:
+        q (_type_): The main queue
+        n (int): The amount of publishers to start
+    """
+
     T = []  # for holding the threads
+    # start n threads, add a client for each
     for i in range(n):
+
         # create client for the thread
         client = mqtt.Client(f"P{i}")
         client.connect("0.0.0.0")
 
+        # construct the thread
         t = threading.Thread(
             target=process_queue_items,
             args=(
@@ -68,7 +88,7 @@ def start_publishers(q, n: int):
                 q,
                 client,
             ),
-        )  # construct the thread
+        )
         T.append(t)  # add thread reference to list for join later
         t.start()  # start the thread
 
@@ -76,6 +96,7 @@ def start_publishers(q, n: int):
 
 
 if __name__ == "__main__":
+
     PUBLISHER_AMOUNT = 10
 
     ctx = mp.get_context("spawn")
