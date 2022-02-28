f1f4e618c927ecd60691d7c1dd7e4e50037347e0
ashlamp08 <dhungana.ashok08@gmail.com>
2022-02-28 17:50:56 +0200

    added slides that need modification

--- metrics.py
+++ metrics.py
@@ -9,10 +9,14 @@ if __name__ == "__main__":
                 rows = conn.read_time_data()
 
                 sum = 0.0
+                diffs = []
                 for row in rows:
+                    diffs.append(float(row[1]) - float(row[0]))
                     sum = sum + float(row[1]) - float(row[0])
 
-                average_dealy = sum/len(rows)
-                print(f"Average delay is : {average_dealy}")
+                average_delay = sum/len(rows)
+                print(f"Average delay is : {average_delay}")
+                print(f"Minumum delay is : {min(diffs)}")
+                print(f"Maximum delay is : {max(diffs)}")
     except KeyboardInterrupt:
         pass
